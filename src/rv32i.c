2380bd8353eb374f5441852afa79e7e6a517cec7
AsgNie <236596459+AsgNie@users.noreply.github.com>
2025-11-08 17:40:02 +0100

    rv32i.c/h added with common functionalities for the RV32I minimal instruction set. NOT UNIT TESTED YET. Assume bugs are present.

---
+++ src/rv32i.c
@@ -0,0 +1,172 @@
+#include "rv32i.h"
+#include <assert.h>
+
+rv32i_instruct_t rv32iDecodeInstructType(int32_t instruct)
+{
+    uint8_t opcode, funct3, funct7;
+
+    opcode = rv32iGetOpcode(instruct);
+
+    switch (opcode)
+    {
+    case RV32I_OPCODE_ALU:
+        funct3 = rv32iGetFunct3(instruct);
+        funct7 = rv32iGetFunct7(instruct);
+        switch (funct3)
+        {
+        case 0b000:
+            switch (funct7)
+            {
+            case 0b0000000:
+                return RV32I_ADD;
+            case 0b0100000:
+                return RV32I_SUB;
+            default:
+                return RV32I_NOT_SUPPORTED;
+            }
+        case 0b001:
+            return RV32I_SLL;
+        case 0b010:
+            return RV32I_SLT;
+        case 0b011:
+            return RV32I_SLTU;
+        case 0b100:
+            return RV32I_XOR;
+        case 0b101:
+            switch (funct7)
+            {
+            case 0b0000000:
+                return RV32I_SRL;
+            case 0b0100000:
+                return RV32I_SRA;
+            default:
+                return RV32I_NOT_SUPPORTED;
+            }
+        case 0b110:
+            return RV32I_OR;
+        case 0b111:
+            return RV32I_AND;
+        default:
+            assert(0); // This condition should be unreachable.
+            return RV32I_NOT_SUPPORTED;
+        }
+        break;
+    case RV32I_OPCODE_ALU_IMM:
+        funct3 = rv32iGetFunct3(instruct);
+        switch (funct3)
+        {
+        case 0b000:
+            return RV32I_ADDI;
+        case 0b010:
+            return RV32I_SLTI;
+        case 0b011:
+            return RV32I_SLTIU;
+        case 0b100:
+            return RV32I_XORI;
+        case 0b110:
+            return RV32I_ORI;
+        case 0b111:
+            return RV32I_ANDI;
+        default:
+            return RV32I_NOT_SUPPORTED;
+        }
+    default:
+        return RV32I_NOT_SUPPORTED;
+    }
+}
+
+int32_t rv32iGenerateImmediate(int32_t instruct)
+{
+    int32_t imm = 0;
+    uint8_t opcode = rv32iGetOpcode(instruct);
+    switch (rv32iOpcodeToOpcodeType(opcode))
+    {
+    case RV32I_OPCODE_TYPE_I:
+        imm = (instruct) >> 20;
+        break;
+    case RV32I_OPCODE_TYPE_S:
+        imm  = (instruct >> 7) & 0x0000001f;
+        imm |= instruct >> 20;
+        break;
+    case RV32I_OPCODE_TYPE_B:
+        imm |= (instruct >> 7 ) & 0x0000001e;    // imm[4:1]
+        imm |= (instruct >> 20) & 0x00000fc0;    // imm[10:5];
+        imm |= (instruct << 4 ) & 0x00000800;    // imm[11]
+        imm |= (instruct >> 19) & 0xfffff000;    // imm[12] with sign extension
+        break;
+    case RV32I_OPCODE_TYPE_U:
+        imm = instruct & 0xfffff000;    // imm[31:12]
+        break;
+    case RV32I_OPCODE_TYPE_J:
+        imm  = (instruct >> 0 ) & 0x000ff000;   // imm[19:12]
+        imm |= (instruct >> 9 ) & 0x00000800;   // imm[11]
+        imm |= (instruct >> 21) & 0x000007fe;   // imm[10:1]
+        imm |= (instruct >> 11) & 0xfff00000;   // imm[20] with sign extension
+        break;
+    case RV32I_OPCODE_TYPE_R:
+        imm = -1;
+        break;
+    case RV32I_OPCODE_TYPE_UNKNOWN:
+        imm = -2;
+        break;
+    default:
+        imm = -3;
+        break;
+    };
+
+    return imm;
+}
+
+uint8_t rv32iGetFunct3(int32_t instruct)
+{
+    return (instruct >> 12) & 0x03;
+}
+
+uint8_t rv32iGetFunct7(int32_t instruct)
+{
+    return (instruct >> 25) & 0x7f;
+}
+
+uint8_t rv32iGetOpcode(int32_t instruct)
+{
+    return (instruct >> 0 ) & 0x7f;
+}
+
+uint8_t rv32iGetRd(int32_t instruct)
+{
+    return (instruct >> 7 ) & 0x1f;
+}
+
+uint8_t rv32iGetRs1(int32_t instruct)
+{
+    return (instruct >> 15) & 0x1f;
+}
+
+uint8_t rv32iGetRs2(int32_t instruct)
+{
+    return (instruct >> 20) & 0x1f;
+}
+
+rv32i_opcodeTypes_t rv32iOpcodeToOpcodeType(uint8_t opcode)
+{
+    switch (opcode)
+    {
+    case RV32I_OPCODE_BRANCH:
+        return RV32I_OPCODE_TYPE_B;
+    case RV32I_OPCODE_LUI:
+        return RV32I_OPCODE_TYPE_U;
+    case RV32I_OPCODE_JAL:
+        return RV32I_OPCODE_TYPE_J;
+    case RV32I_OPCODE_JALR:     // Fallthrough
+    case RV32I_OPCODE_LOAD:     // Fallthrough
+    case RV32I_OPCODE_ALU_IMM:  // Fallthrough
+    case RV32I_OPCODE_ENV:
+        return RV32I_OPCODE_TYPE_I;
+    case RV32I_OPCODE_ALU:
+        return RV32I_OPCODE_TYPE_R;
+    case RV32I_OPCODE_STORE:
+        return RV32I_OPCODE_TYPE_S; 
+    default:
+        return RV32I_OPCODE_TYPE_UNKNOWN;
+    }
+}
---
+++ src/rv32i.h
@@ -0,0 +1,53 @@
+#ifndef RV32I_H
+#define RV32I_H
+#include <stdint.h>
+
+/*
+Collection of defines and functionalities generally useable across multiple types of RISC-V RV32I instruction set implementations.
+*/
+
+/*
+RV32I Base Instruction Set opcodes as given in The RISC-V Instruction Set Manual
+Volume I, Version 20250508, Chapter 35: RV32/64G Instruction Set Listings.
+*/
+#define RV32I_OPCODE_BRANCH     (0b1100011)
+#define RV32I_OPCODE_LUI        (0b0110111)
+#define RV32I_OPCODE_JAL        (0b1101111)
+#define RV32I_OPCODE_JALR       (0b1100111)
+#define RV32I_OPCODE_LOAD       (0b0000011)
+#define RV32I_OPCODE_STORE      (0b0100011)
+#define RV32I_OPCODE_ALU_IMM    (0b0010011)
+#define RV32I_OPCODE_ALU        (0b0110011)
+#define RV32I_OPCODE_FENCE      (0b0110011)
+#define RV32I_OPCODE_ENV        (0b1110011)
+
+/* All supported rv32i instructions including 3 formats for unsupported instructions to differentiate between instructions where funct3 and funct7
+should be evaluated for error handling. */
+typedef enum rv32i_instruct_t
+{
+    RV32I_NOT_SUPPORTED = -1, RV32I_ADD = 0, RV32I_ADDI, RV32I_AND, RV32I_ANDI, RV32I_AUIPC, RV32I_BEQ, RV32I_BGE, RV32I_BGEU, RV32I_BLT,
+    RV32I_BLTU, RV32I_BNE, RV32I_ECALL, RV32I_JAL, RV32I_JALR, RV32I_LB, RV32I_LBU, RV32I_LH, RV32I_LHU, RV32I_LUI, RV32I_LW, RV32I_OR,
+    RV32I_ORI, RV32I_SB, RV32I_SH, RV32I_SLL, RV32I_SLLI, RV32I_SLT, RV32I_SLTI, RV32I_SLTIU, RV32I_SLTU, RV32I_SRA, RV32I_SRAI, RV32I_SRL,
+    RV32I_SRLI, RV32I_SUB, RV32I_SW, RV32I_XOR, RV32I_XORI, 
+} rv32i_instruct_t;
+
+typedef enum rv32i_opcodeTypes_t
+{
+    RV32I_OPCODE_TYPE_UNKNOWN = -1, RV32I_OPCODE_TYPE_R = 0, RV32I_OPCODE_TYPE_I, RV32I_OPCODE_TYPE_S,
+    RV32I_OPCODE_TYPE_B, RV32I_OPCODE_TYPE_U, RV32I_OPCODE_TYPE_J
+} rv32i_opcodeTypes_t;
+
+
+enum rv32i_instruct_t rv32iDecodeInstructType(int32_t instruct);
+int32_t rv32iGenerateImmediate(int32_t instruct);
+uint8_t rv32iGetFunct3(int32_t instruct);
+uint8_t rv32iGetFunct7(int32_t instruct);
+uint8_t rv32iGetOpcode(int32_t instruct);
+uint8_t rv32iGetRd (int32_t instruct);
+uint8_t rv32iGetRs1(int32_t instruct);
+uint8_t rv32iGetRs2(int32_t instruct);
+enum rv32i_opcodeTypes_t rv32iOpcodeToOpcodeType (uint8_t opcode);
+
+
+
+#endif //RV32I_H
